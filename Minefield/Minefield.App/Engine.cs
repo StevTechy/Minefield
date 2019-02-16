5ea8b320939fac1ef369cef54ad1ac1343339627
Steven <stevenms@outlook.com>
2019-02-16 23:16:51 +0000

    Added alternative controls for movement, closing window

--- Minefield/Minefield.App/Engine.cs
+++ Minefield/Minefield.App/Engine.cs
@@ -7,7 +7,7 @@ namespace Minefield.App
     {
         public void Start(IBoard board)
         {
-            board.Setup(8, 8);
+            board.Setup(8,8);
 
             while (true)
             {
@@ -16,32 +16,36 @@ namespace Minefield.App
                 switch (input.Key)
                 {
                     case ConsoleKey.UpArrow:
+                    case ConsoleKey.W:
                         {
                             board.MoveUp();
                             break;
                         }
                     case ConsoleKey.DownArrow:
+                    case ConsoleKey.S:
                         {
                             board.MoveDown();
                             break;
                         }
                     case ConsoleKey.LeftArrow:
+                    case ConsoleKey.A:
                         {
                             board.MoveLeft();
                             break;
                         }
                     case ConsoleKey.RightArrow:
+                    case ConsoleKey.D:
                         {
                             board.MoveRight();
                             break;
                         }
                     case ConsoleKey.Enter:
+                    case ConsoleKey.Escape:
                         {
                             return;
                         }
                 }
             }
-
         }
 
         public void End()
